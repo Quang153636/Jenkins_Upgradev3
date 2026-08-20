c1e75655e745ed3048b04330d26f0ec806b63434
Quang153636 <quang153636@gmail.com>
2026-08-20 20:56:21 +0700

    Putting Failed Test Case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,7 +26,8 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
-
+  
+  @Test
   public void intentionally_Failed() {
     assertTrue(false);
   }
