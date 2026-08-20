1101a1cb8a2f1264700cb093f35c71c17fb371d8
Quang153636 <quang153636@gmail.com>
2026-08-20 21:32:58 +0700

    Removing TestCase

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -27,10 +27,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone), containsString(someone));
   }
   
-  @Test
-  public void intentionally_Failed() {
-    assertTrue(true);
-  }
+  
   
 
   @Test
